61df5cae780e701bfb02b47b23dd8a1a33f59231
tianpian <940581988@qq.com>
2016-10-20 17:49:16 +0800

    文件上传

---
+++ application/admin/controller/Upload.php
@@ -0,0 +1,22 @@
+<?php
+// +----------------------------------------------------------------------
+// | tpadmin [a web admin based ThinkPHP5]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 tianpian All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: tianpian <tianpian0805@gmail.com>
+// +----------------------------------------------------------------------
+
+namespace app\admin\controller;
+
+use app\admin\Controller;
+
+class Upload extends Controller
+{
+    public function index()
+    {
+        return $this->view->fetch();
+    }
+}
