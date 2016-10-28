648d65a9b262f83568e2f94ab30e78fb0b812ee6
tianpian <940581988@qq.com>
2016-10-28 16:52:59 +0800

    修复 firefox 里点击不能弹框的bug

--- application/admin/extra/qiniu.php
+++ application/admin/extra/qiniu.php
@@ -11,8 +11,8 @@
 
 return [
     // 七牛云存储配置信息
-    "accessKey" => "",
-    "secretKey" => "",
-    "bucket"    => "", // 存储空间
-    "domain"    => "", // 访问域名
+    "accessKey" => "bKuXyGUf1qrgqwyfvcWlLTh2ublMhWlIwVUXFHND",
+    "secretKey" => "79_qLvHCfTIqOywmUuri-diSV0P_KVXvCN4tgmJ8",
+    "bucket"    => "rybbaby", // 存储空间
+    "domain"    => "http://7j1y9l.com1.z0.glb.clouddn.com/", // 访问域名
 ];
