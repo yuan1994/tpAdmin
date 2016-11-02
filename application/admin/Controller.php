933b95d0af30889dd56628eafbec40ad033a9742
yuan1994 <tianpian0805@gmail.com>
2016-11-02 22:38:33 +0800

    增强过滤器功能，增加列表可选返回值

--- application/admin/Controller.php
+++ application/admin/Controller.php
@@ -210,4 +210,148 @@ class Controller
             }
         }
     }
+
+    /**
+     * 过滤禁止操作某些主键
+     * @param $filterData
+     * @param string $error
+     * @param string $method
+     * @param string $key
+     */
+    protected function filterId($filterData, $error = '该记录不能执行此操作', $method = 'in_array', $key = 'id')
+    {
+        $data = $this->request->param();
+        if (!isset($data[$key])) {
+            throw new Exception('缺少必要参数');
+        }
+        $ids = is_array($data[$key]) ? $data[$key] : explode(",", $data[$key]);
+        foreach ($ids as $id) {
+            switch ($method) {
+                case '<':
+                case 'lt':
+                    $ret = $id < $filterData;
+                    break;
+                case '>':
+                case 'gt':
+                    $ret = $id < $filterData;
+                    break;
+                case '=':
+                case 'eq':
+                    $ret = $id == $filterData;
+                    break;
+                case '!=':
+                case 'neq':
+                    $ret = $id != $filterData;
+                    break;
+                default:
+                    $ret = call_user_func_array($method, [$id, $filterData]);
+                    break;
+            }
+            if ($ret) {
+                throw new Exception($error);
+            }
+        }
+    }
+
+    /**
+     * 根据表单生成查询条件
+     * 进行列表过滤
+     *
+     * 过滤条件
+     * $map['_table']       可强制设置表名前缀
+     * $map['_relation']    可强制设置关联模型预载入(需在模型里定义)
+     * $map['_field']       可强制设置字段
+     * $map['_order_by']    可强制设置排序字段(field asc|desc[,filed2 asc|desc...]或者false)
+     * $map['_paginate']    是否开启分页，传入false可以关闭分页
+     *
+     * @param object $model     数据对象
+     * @param array $map        过滤条件
+     * @param string $field     查询的字段
+     * @param string $sortBy    排序
+     * @param boolean $asc      是否正序
+     * @param boolean $return   是否返回数据，返回数据时返回paginate对象，不返回时直接模板赋值
+     * @param boolean $paginate 是否开启分页
+     */
+    protected function datalist($model, $map, $field = '*', $sortBy = '', $asc = false, $return = false, $paginate = true)
+    {
+        // 排序字段 默认为主键名
+        $order = $this->request->param('_order') ?: (empty($sortBy) ? $model->getPk() : $sortBy);
+
+        // 接受 sort参数 0 表示倒序 非0都 表示正序
+        $sort = $this->request->param('_sort') !== '' ?
+            ($this->request->param('_sort') == 'asc' ? 'asc' : 'desc') :
+            ($asc ? 'asc' : 'desc');
+
+        // 设置关联预载入
+        if (isset($map['_relation'])) {
+            $model = $model::with($map['_relation']);
+        }
+
+        // 设置字段
+        if (isset($map['_field'])) {
+            $field = $map['_field'];
+        }
+
+        // 设置有$map['_controller']表示存在关联模型
+        if (isset($map['_table'])) {
+            // 给排序字段强制加上表名前缀
+            if (strpos($order, ".") === false) {
+                $order = $map['_table'] . "." . $order;
+            }
+            // 给字段强制加上表名前缀
+            $_field = is_array($field) ? $field : explode(",", $field);
+            foreach ($_field as &$v) {
+                if (strpos($v, ".") === false) {
+                    $v = preg_replace("/([^\s\(\)]*[a-z0-9\*])/", $map['_table'] . '.$1', $v, 1);
+                }
+            }
+            $field = implode(",", $_field);
+        }
+
+        // 设置排序字段 防止表无主键报错
+        $order_by = $order ? "{$order} {$sort}" : false;
+        if (isset($map['_order_by'])) {
+            $order_by = $map['_order_by'];
+        }
+
+        // 是否开启分页
+        $paginate = isset($map['_paginate']) ? boolval($map['_paginate']) : $paginate;
+
+        // 删除设置属性的字段
+        unset($map['_table'], $map['_relation'], $map['_field'], $map['_order_by'], $map['_paginate']);
+
+        if ($paginate) {
+            // 分页查询
+
+            // 每页数据数量
+            $listRows = $this->request->param('numPerPage') ?: Config::get("paginate.list_rows");
+
+            $list = $model->field($field)->where($map)->order($order_by)->paginate($listRows, false, ['query' => $this->request->get()]);
+
+            if ($return) {
+                // 返回值
+                return $list;
+            } else {
+                // 模板赋值显示
+                $this->view->assign('list', $list);
+                $this->view->assign("count", $list->total());
+                $this->view->assign("page", $list->render());
+                $this->view->assign('numPerPage', $list->listRows());
+            }
+        } else {
+            // 不开启分页查询
+            $list = $model->field($field)->where($map)->order($order_by)->select();
+
+            if ($return) {
+                // 返回值
+                return $list;
+            } else {
+                // 模板赋值显示
+                $this->view->assign('list', $list);
+                $this->view->assign("count", count($list));
+                $this->view->assign("page", '');
+                $this->view->assign('numPerPage', 0);
+            }
+        }
+    }
 }
